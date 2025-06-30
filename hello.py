f21908fc28a4d8cd79a2a01443d05054d4e91326
Mg Phone <khantalfred832@gmail.com>
2025-06-30 16:34:46 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum voluptatum natus, 
-nisi corporis inventore, eius magni maxime tempore eum voluptates 
-minus porro ipsam odio itaque? Dignissimos ullam dolores eos quaerat?
+# Student Name: PhoneMinKhant
+# Date: 2025-06-28
+
+print("Hello, Python World!")
